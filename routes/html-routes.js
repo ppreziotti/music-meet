a4e92bba8103f571abc7214f316ba4451a216bbf
ppreziotti <patpreziotti@gmail.com>
2017-09-09 10:54:11 -0400

    added dependencies, ejs templating, passport login/signup setup in progress

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,7 @@
-module.exports = function(app) {
+module.exports = function(app, passport) {
 	// Main page route
 	app.get('/', function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/", "index.html"));
+		res.render('index.ejs');
 	});
 	// Discover page route
 	app.get('/discover', function(req, res) {
@@ -9,10 +9,30 @@ module.exports = function(app) {
 	});
 	// Login page route
 	app.get('/login', function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/", "login.html"));
+		res.render('login.ejs', {message: req.flash('loginMessage')});
+	});
+	// Logout route
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+	// Profile page route - can only view once logged in
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render(profile.ejs, {
+			user: req.user
+		});
 	});
 	// Signup page route
 	app.get('/signup', function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/", "signup.html"));
+		res.render('signup.ejs', {message: req.flash('signupMessage')});
 	});
+	// Function to determine if a user is logged in or not
+	function isLoggedIn(req, res, next) {
+		// Allow the user to continue if authenticated in the session
+		if (req.isAuthenticated()) {
+			return next();
+		}
+		// Otherwise, redirect to the home page for login
+		res.redirect('/');
+	}
 }
--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 // Dependencies
 var bodyParser = require('body-parser');
-// var db = require('./models');
+var cookieParser = require('cookie-parser');
 var express = require('express');
-var path = require('path');
+var flash = require('connect-flash');
 var mongoose = require('mongoose');
 mongoose.Promise = Promise;
+var morgan = require('morgan');
+var passport = require('passport');
+var path = require('path');
+var session = require('express-session');
 
 var User = require('./models/User.js');
 
 // Set up the express app 
 var app = express();
 var PORT = process.env.PORT || 8080;
-
+app.use(morgan('dev'));
+app.use(cookieParser());
 // Enable the express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+// Passport setup
+// require('./config/passport.js')(passport);
+app.use(session({secret: 'musicmeetsecret'}));
+app.use(passport.initialize());
+app.use(passport.session());
+// Flash messages for login and signup
+app.use(flash());
+
+// Use ejs for templating
+app.set('view engine', 'ejs');
 
 // Path for static content
 app.use(express.static(path.join(__dirname, 'public')));
@@ -35,7 +50,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
 // Import the API and HTML routes
-// require('./routes/api-routes.js')(app);
+// require('./routes/api-routes.js')(app, passport);
 require('./routes/html-routes.js')(app);
 
 app.listen(PORT, function() {
